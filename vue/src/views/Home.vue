9cc9a48d51bafe95f5eee908c2dd3a40c5a00bc0
Myron Law <myron@techelevator.com>
2023-04-07 13:01:24 -0400

    Initial push of final capstone starter code

---
+++ vue/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>If you are seeing this, you are authenticated.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
